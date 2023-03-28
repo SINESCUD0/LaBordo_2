7783ba58c3cb79fd114217b699b25ce8f7c8a164
iManod2k <contacto.ayman@gmail.com>
2023-03-28 13:07:09 +0200

    He añadido la pestaña que pide USUARIO y CONTRASEÑA

--- app/src/main/java/com/example/labordo/MainActivity.java
+++ app/src/main/java/com/example/labordo/MainActivity.java
@@ -2,11 +2,16 @@
 
 package com.example.labordo;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.example.labordo.fragmentos.Tab1Fragment;
 import com.google.android.material.tabs.TabLayout;
@@ -15,6 +20,8 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static String usuario;
+    private static String contraseña;
     TabLayout tabLayout;
     ViewPager2 viewPager2;
     MiVisualizadorDePantalla miVisualizadorDePantalla;
@@ -23,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        alertDialogConInput();
 
         tabLayout = findViewById(R.id.Tab_Layout);
         viewPager2 = findViewById(R.id.Vista_Pagina);
@@ -53,5 +62,38 @@ public class MainActivity extends AppCompatActivity {
                 tabLayout.getTabAt(position).select();
             }
         });
+    }
+
+
+    private void alertDialogConInput() {
+
+        LinearLayout linearLayout = new LinearLayout(this.getApplicationContext());
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("Inicio de sesión:");
+
+        EditText input_usuario = new EditText(this);
+        input_usuario.setHint("Usuario");
+        input_usuario.setInputType(InputType.TYPE_CLASS_TEXT);
+
+
+        EditText input_contrasena = new EditText(this);
+        input_contrasena.setHint("Contraseña");
+        input_contrasena.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+
+        linearLayout.addView(input_usuario);
+        linearLayout.addView(input_contrasena);
+
+        alertDialogBuilder.setView(linearLayout);
+        alertDialogBuilder.setPositiveButton("Enviar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                usuario = input_usuario.getText().toString();
+            }
+        });
+        alertDialogBuilder.setNegativeButton("Cancelar", null);
+
+        alertDialogBuilder.show();
     }
 }
