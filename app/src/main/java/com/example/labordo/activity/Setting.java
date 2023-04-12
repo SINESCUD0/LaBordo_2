2bf53d430359ed6f1969994693fc96aa6441e6d0
iManod2k <contacto.ayman@gmail.com>
2023-04-12 14:30:42 +0200

    * Setting funciona. Cuando las contaraseñascoinciden, sale a la actividad anterior

--- app/src/main/java/com/example/labordo/activity/Setting.java
+++ app/src/main/java/com/example/labordo/activity/Setting.java
@@ -4,6 +4,10 @@ package com.example.labordo.activity;
 import android.app.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import com.example.labordo.R;
 
 public class Setting extends AppCompatActivity {
@@ -12,5 +16,24 @@ public class Setting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        findViewById(R.id.botonAceptarSetting).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText et1 = (EditText)findViewById(R.id.correoUsuarioRecuperar1);;
+                EditText et2 = (EditText)findViewById(R.id.correoUsuarioRecuperar2);
+
+                String pass1 = et1.getText().toString();
+                String pass2 = et2.getText().toString();
+
+                if(pass1.equals(pass2)){
+                    Toast.makeText(view.getContext(), "Contraseña cambiada", Toast.LENGTH_LONG).show();
+                    // Hacer cosas
+                    finish();
+                }else{
+                    Toast.makeText(view.getContext(), "No coinciden. Inténtelo de nuevo", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
