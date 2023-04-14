23ef63d41dbc9bcdc470ee470c72631fa45fe44e
SINESCUD0 <ivangarciaperalta11@gmail.com>
2023-04-14 13:18:29 +0200

    he conseguido poder añadir fotos de perfil, pero aun no se guarda la foto

--- app/src/main/java/com/example/labordo/activity/Perfil.java
+++ app/src/main/java/com/example/labordo/activity/Perfil.java
@@ -15,9 +15,6 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.labordo.R;
-
-import java.util.Objects;
-
 public class Perfil extends AppCompatActivity {
 
     Button botonFoto;
