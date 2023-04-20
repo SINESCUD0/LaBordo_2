7495de1fea26850396b251eb552331068edf7450
SINESCUD0 <ivangarciaperalta11@gmail.com>
2023-04-20 11:48:49 +0200

    He renombrado el prueba activity

--- app/src/main/java/com/example/labordo/activity/LoginActivity.java
+++ app/src/main/java/com/example/labordo/activity/LoginActivity.java
@@ -1,72 +1,157 @@
 package com.example.labordo.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.labordo.R;
-import com.example.labordo.base_datos.BaseDatosGeneral;
 import com.example.labordo.usuarios.Main_Alumnado;
 import com.example.labordo.usuarios.Main_Profesorado;
 
-public class LoginActivity extends AppCompatActivity {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
-    EditText correoUsuario;
-    EditText passwordUsuario;
-    Cursor cv;
-    String datos;
+public class LoginActivity extends AppCompatActivity {
+    //PARA CONECTARTE A LA BASE DE DATOS
+    private static final String DATABASE_URL = "jdbc:mysql://192.168.1.38:3306/labordo?useUnicode=true&characterEncoding=UTF-8\"";
+
+    //USUARIO PARA INICIAR SESION EN LA BASE DE DATOS
+    private static final String USER = "root";
+
+    //CONTRASEÑA PARA INICIAR SESION EN EL USUARIO ROOT
+    private static final String PASSWORD = "L4b0rd0#";
+
+    //EDIT TEXT PARA PODER RECOGER LO QUE INTRODUCE EL USUARIO DENTRO DEL LAYOUT LOGIN
+    EditText correoUsuario, passwordUsuario;
+    Button iniciar;
+
+    String tipo1 = "profesor";
+    String tipo2 = "estudiante";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        correoUsuario = (EditText) findViewById(R.id.correoUsuario);
-        passwordUsuario = (EditText) findViewById(R.id.passwordUsuario);
+        //INDICO QUE ES CORREO, PASSWORD Y EL BOTON DENTRO DEL LAYOUT LOGIN
+        correoUsuario = findViewById(R.id.correoUsuario);
+        passwordUsuario = findViewById(R.id.passwordUsuario);
+        iniciar = findViewById(R.id.login);
+
+        //CUANDO DEMOS AL BOTON DE INICIAR SE EJECUTARA EL SIGUIENTE METODO
+        iniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Send objSend = new Send();
+                objSend.execute();
+            }
+        });
     }
 
-    public void iniciarSesion(View view){
-        BaseDatosGeneral dbHelper = new BaseDatosGeneral(this);
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-        String correo = correoUsuario.getText().toString();
-        String password = passwordUsuario.getText().toString();
-        cv = db.rawQuery("SELECT correoUsuario, passwordUsuario, tipoCuenta FROM t_usuarios WHERE correoUsuario = '"
-                +correo+"' AND passwordUsuario = '"+password+"'",null);
-        try {
-            if (cv.moveToFirst()) {
-                String corr = cv.getString(0);
-                String pass = cv.getString(1);
-                String tipo = cv.getString(2);
-                if (correo.equals(corr) && password.equals(pass)) {
+    class Send extends AsyncTask<Void, Void, Void> {
 
-                    switch(tipo){
-                        case "Profesor": startActivity(new Intent(this, Main_Profesorado.class));break;
-                        case "Alumno": startActivity(new Intent(this, Main_Alumnado.class));break;
+        //ESTA VARIABLE (MSG) LA UTILIZAMOS PARA EN CASO DE FALLO TE MUESTRE EN EL TOAST EL FALLO QUE DA
+        String msg = "";
+
+        //COGEMOS LOS VALORES DE LOS EDIT TEXT Y LOS PASAMOS A STRING ESOS VALORES
+        String correo1 = correoUsuario.getText().toString();
+        String password1 = passwordUsuario.getText().toString();
+
+        @Override
+        protected void onPreExecute(){
+            Toast.makeText(LoginActivity.this,"Iniciando Sesion", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try{
+                Class.forName("com.mysql.jdbc.Driver"); //PILLAMOS LA INFORMACION DEL PAQUETE
+                Connection conn = DriverManager.getConnection(DATABASE_URL,USER, PASSWORD); //NOS CONECTAMOS A LA BASE DE DATOS
+                if(conn == null){
+                    //SI NO CONSIGUES CONECTARTE A LA BASE DE DATOS
+                    msg = "La conexion va mal";
+                }else{
+                    //SI CONSIGUE CONECTARSE A LA BASE DE DATOS QUE EJECUTE LA SIGUIENTE SENTENCIA
+                    String query = "SELECT 'estudiante' as tipo FROM estudiante WHERE correo = ? " +
+                            "UNION " +
+                            "SELECT 'profesor' FROM profesor WHERE correo = ?";
+                    PreparedStatement statement = conn.prepareStatement(query);
+                    statement.setString(1, correo1);
+                    statement.setString(2, correo1);
+                    ResultSet rs = statement.executeQuery();
+                    //LEE LA CONSULTA Y MIRA DE QUE TIPO ES EL CORREO
+                    if (rs.next()) {
+                        String tipo = rs.getString("tipo");
+                        //SI EL CORREO INTRODUCIDO ES DE UN PROFESOR HARA LO SIGUIENTE
+                        if(tipo.equals(tipo1)){
+                            String query2 = "SELECT * FROM profesor WHERE correo = ? AND contrasenia = ?";
+                            PreparedStatement statement2 = conn.prepareStatement(query2);
+                            statement2.setString(1, correo1);
+                            statement2.setString(2, password1);
+                            ResultSet rs2 = statement2.executeQuery();
+
+                            msg = "¡Inicio de sesión exitoso!";
+
+                            Intent i = new Intent(LoginActivity.this, Main_Profesorado.class);
+                            startActivity(i);
+
+                            statement2.close();
+                            rs2.close();
+
+                        }
+                        //SI EL CORREO INTRODUCIDO ES DE UN ESTUDIANTE HARA LO SIGUIENTE
+                        else if (tipo.equals(tipo2)) {
+                            String query2 = "SELECT * FROM estudiante WHERE correo = ? AND contrasenia = ?";
+                            PreparedStatement statement2 = conn.prepareStatement(query2);
+                            statement2.setString(1, correo1);
+                            statement2.setString(2, password1);
+                            ResultSet rs2 = statement2.executeQuery();
+
+                            if(rs2.next()){
+                                msg = "¡Inicio de sesión exitoso!";
+
+                                Intent i = new Intent(LoginActivity.this, Main_Alumnado.class);
+                                startActivity(i);
+                            }
+                            statement2.close();
+                            rs2.close();
+                        }
+                    } else {
+                        //SI INTRODUCES MAL LOS DATOS SALDRA EL SIGUIENTE MENSAJE
+                        msg = "No has introducido bien los datos";
                     }
-                    //Intent i = new Intent(this, Main_Profesorado.class);
-                    //startActivity(i);
-                    datos = corr;
-                    correoUsuario.setText("");
-                    passwordUsuario.setText("");
-                }
-            }
-            else {
-                Toast.makeText(this, "Datos incorrectos", Toast.LENGTH_LONG).show();
-            }
 
-        } catch (Exception e) {
-            Toast.makeText(this, "Error" + e.getMessage(), Toast.LENGTH_LONG).show();
+                    statement.close();
+                    rs.close();
+                }
+
+                conn.close();
+
+            }catch (Exception e){
+                //SI FALLA ALGO EN EL CODIGO SALDRA LA SIGUIENTE EXCEPTION
+                msg = "La conexion va mal excepcion" + e.getMessage();
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid){
+            Toast.makeText(LoginActivity.this, msg, Toast.LENGTH_SHORT).show();
         }
     }
 
+    //SI AUN NO TIENES UNA CUENTA AL DAR AL BOTON REGISTRAR HARA LO SIGUIENTE
     public void registrarse(View view){
-        Intent i = new Intent(this, PruebaRegistro.class);
+        Intent i = new Intent(this, RegistroActivity.class);
         startActivity(i);
     }
 }
--- app/src/main/java/com/example/labordo/splash_screen/SplashScreen.java
+++ app/src/main/java/com/example/labordo/splash_screen/SplashScreen.java
@@ -10,7 +10,6 @@ import android.widget.TextView;
 
 import com.example.labordo.R;
 import com.example.labordo.activity.LoginActivity;
-import com.example.labordo.activity.PruebaLogin;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -37,7 +36,7 @@ public class SplashScreen extends Activity {
         TimerTask tarea = new TimerTask() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this, PruebaLogin.class);
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
